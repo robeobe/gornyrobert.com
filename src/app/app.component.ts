e2ac347b50da369419fb21c35151f03d9be69dfe
robeobe <gornyrobert@gmail.com>
2024-11-17 22:35:47 +0100

    wolf in w png

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'new-gr-main';
-  imagePath = 'assets/images/futuristic-apocalyptic-wolf.jpg';
 }
